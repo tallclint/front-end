9f760dc9db105d9a6632d5ee288040c3197820c8
Ryan <snorekr@gmail.com>
2021-10-18 13:17:51 -0400

    Added functionality to form to update state on submit

--- plants/src/Login.js
+++ plants/src/Login.js
@@ -6,23 +6,34 @@ export default function Login() {
     
     const initialLoginValues = {
         username: "",
-        password: ""
+        password: "",
     }
     const [loginValues, setLoginValues] = useState(initialLoginValues)
     
-    
-    const validate = () => {
-    
+    // YUP validation on submit //
+    const validate = e => {
+        
     }
     
     const change = e => {
-        
-    
+        const { name, value } = e.target
+        // Update state with the changes
+        setLoginValues({ ...loginValues, [name]: value})
     } 
+
+    const submit = e => {
+        e.preventDefault()
+        const loginCredentials = {
+            username: loginValues.username,
+            password: loginValues.password,
+        }
+        validate(loginCredentials)        
+    }
+
     return (
         <>
             <h1>Login</h1>
-            <form id="login-form">
+            <form id="login-form" onSubmit={submit}>
                 <label>
                     <input
                         type="text"
@@ -40,6 +51,7 @@ export default function Login() {
                         placeholder="Enter password"
                     />
                 </label>
+                <button onClick={validate}>Login</button>
             </form>
         </>
     )
