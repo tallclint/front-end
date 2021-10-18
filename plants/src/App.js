fb13991f18e492bea7d6e98e07036372f2557849
RoseGabriely <rosegabriely14@gmail.com>
2021-10-18 15:33:50 -0400

    organized files into folders

--- plants/src/App.js
+++ plants/src/App.js
@@ -1,9 +1,9 @@
-import { Link, Route, Switch } from 'react-router-dom';
-import './App.css';
+import { Link, Route, Switch } from "react-router-dom";
+import "./css/App.css";
 
 //*Custom Components
-import HomePage from './HomePage';
-import Login from './Login';
+import HomePage from "./components/HomePage";
+import Login from "./components/Login";
 
 function App() {
   return (
@@ -11,19 +11,18 @@ function App() {
       <header>
         <nav>
           <h1>Water My Plants</h1>
-          <Link to='/'>Home</Link>
-          <Link to='/sign-in'>Sign In</Link>
+          <Link to="/">Home</Link>
+          <Link to="/sign-in">Sign In</Link>
         </nav>
       </header>
       <Switch>
-        <Route path='/sign-in'>
+        <Route path="/sign-in">
           <Login />
         </Route>
-        <Route path='/'>
+        <Route path="/">
           <HomePage />
         </Route>
       </Switch>
-      
     </div>
   );
 }
---
+++ plants/src/components/Login.js
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import * as yup from "yup";
+import { yupLoginSchema as schema } from "../yup/yupLoginSchema";
+
+export default function Login() {
+  const initialLoginValues = {
+    username: "",
+    password: "",
+  };
+  const [loginValues, setLoginValues] = useState(initialLoginValues);
+
+  // VALIDATION //
+  const validate = (credentials) => {
+    // Validate / authenticate credentials onSubmit
+  };
+  // ON CHANGE //
+  const change = (e) => {
+    const { name, value } = e.target;
+    // Update state with the changes
+    setLoginValues({ ...loginValues, [name]: value });
+  };
+  // ON SUBMIT //
+  const submit = (e) => {
+    e.preventDefault();
+    const loginCredentials = {
+      username: loginValues.username,
+      password: loginValues.password,
+    };
+    validate(loginCredentials);
+  };
+
+  return (
+    <>
+      <h1>Login</h1>
+      <form id="login-form" onSubmit={submit}>
+        <label>
+          <input
+            type="text"
+            name="username"
+            value={loginValues.username}
+            onChange={change}
+            placeholder="Enter username"
+          />
+        </label>
+        <label>
+          <input
+            type="password"
+            name="password"
+            onChange={change}
+            value={loginValues.password}
+            placeholder="Enter password"
+          />
+        </label>
+        <button onClick={validate}>Login</button>
+      </form>
+    </>
+  );
+}
